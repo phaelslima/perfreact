21e3ad6408f46efa8676a70d85dd19cf0a4011fd
Raphael Lima <phaelslima@gmail.com>
2022-04-27 06:06:58 -0300

    feat: add productItem and searchResults components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,38 @@
 import type { NextPage } from 'next'
+import { FormEvent, useState } from 'react'
+import { SearchResults } from '../components/SearchResults'
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState('')
+  const [results, setReults] = useState([])
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault()
+
+    if (!search.trim()) return
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json()
+
+    setReults(data)
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
   )
 }
 
